889d4cd2a81045481ee52a78000e7048fd8e06da
Adedoyin Seyi Emmanuel <53402704+Nuelsville@users.noreply.github.com>
2023-09-09 17:36:30 +0000

    newer updates

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal'); // Import qrcode-terminal
-
+const qrcode = require('qrcode');
 const client = new Client();
 
 client.on('qr', (qr) => {
-    // Display the QR code as text in the console
-    qrcode.generate(qr, { small: true }); // Display the QR code as a terminal output
-    console.log('Scan the QR code with your WhatsApp mobile app');
+    // Generate and display the QR code as text in the console
+    qrcode.toString(qr, { type: 'terminal' }, (err, url) => {
+        if (err) {
+            console.error('Error generating QR code:', err);
+            return;
+        }
+        console.log(url);
+    });
+    console.log('hello')
 });
+
+// client.on('ready', () => {
+//     console.log('Client is ready');
+// });
 
 client.on('ready', () => {
     console.log('Client is ready');
+
+    // Send a message when the client is ready
+    const phoneNumber = '2347010790895@c.us'; // Replace with the recipient's phone number
+    const message = 'Your message goes here';
+
+    client.sendMessage(phoneNumber, message)
+        .then((response) => {
+            console.log('Message sent successfully:', response);
+        })
+        .catch((error) => {
+            console.error('Error sending message:', error);
+        });
 });
 
 client.on('message', (message) => {
